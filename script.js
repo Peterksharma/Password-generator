67e7075bb9403941704d450b01c961e2606b21bd
Peter Sharma <peterksharma@gmail.com>
2023-04-05 16:02:58 -0500

    updated comments in the JS

--- script.js
+++ script.js
@@ -28,11 +28,7 @@ var charLibrary = '';
 console.log('Characters sets added')
 
 //Function Junction
-//-----------------
-
-
-
-
+//-----------------------------------------------------------------------------------------
 //Builds the password Length. Breaks if numbers are not entered or the value is <8 and >128.
 function passwordLength(minPasswordLength, maxPasswordLenth) {
   var lengthInt;
@@ -59,66 +55,65 @@ function passwordLength(minPasswordLength, maxPasswordLenth) {
 
 //Building the Lower Case Library if the user chooses to add then adding it to the CharLibrary; or excludes and lets user know
 function lowerCaseLibrary () {
-  var lowerCaseInput = confirm('Would you like lower case letters to your password?')
+  var lowerCaseInput = confirm('Would you like lower case letters to your password?') // Asks user if they want to add Lower Case letters
   if (lowerCaseInput == true){
-    alert('Lower case letters have been added.');
+    alert('Lower case letters have been added.'); // Lets user know they have been added
     charLibrary += alphabetLowerCaseLibrary; // Adding the lowercase letters to the carLibary where characters will be stored to recall.
   }
   else {
-    alert('Lower case letters have not beed added.')
+    alert('Lower case letters have not beed added.') // Lets user know they have not been added.
   }
-  console.log('Lower case option has been decided.');
+  console.log('Lower case option has been decided.'); // Logs to verify option has been chosen or denied by the user
   return lowerCaseInput;
 }
 
 //Building the Upper Case Library if the user chooses to add then adding it to the CharLibrary; or excludes and lets user know
 function upperCaseLibrary () {
-  var upperCaseInput = confirm('Would you like upper case letters in your password?')
+  var upperCaseInput = confirm('Would you like upper case letters in your password?') // Asks user if they want to add Upper Case letters
   if (upperCaseInput == true){
-    alert('Upper Case letters have been added.')
+    alert('Upper Case letters have been added.') // Lets user know they have been added
     charLibrary += alphabetUpperCaseLibrary;
   }
   else {
-    alert('Upper Case letters have not been added.')
+    alert('Upper Case letters have not been added.') // Lets user know they have not been added.
   }
-  console.log('Upper case option has been decided');
+  console.log('Upper case option has been decided'); // Logs to verify option has been chosen or denied by the user
   return upperCaseInput;
 }
 
 //Building the Numeric Library if the user chooses to add then adding it to the CharLibrary; or excludes and lets user know
 function numericLibrary () {
-  var numericInput = confirm('Would you like numbers in your password?')
+  var numericInput = confirm('Would you like numbers in your password?') // Asks user if they want to add numbers
   if (numericInput == true){
-    alert('Numbers have been added.')
+    alert('Numbers have been added.') // Lets user know they have been added
     charLibrary += numericOptionsLibrary;
   }
   else {
-    alert('Numbers have not been added.')
+    alert('Numbers have not been added.') // Lets user know they have not been added.
   }
-  console.log('Number option has been decided.');
+  console.log('Number option has been decided.'); // Logs to verify option has been chosen or denied by the user
   return numericInput;
 }
 
 //Building the Symbol Library if the user chooses to add then adding it to the CharLibrary; or excludes and lets user know
 function symbolLibrary () {
-  var symbolInput = confirm ('Would you like symbols in your password?')
+  var symbolInput = confirm ('Would you like symbols in your password?') // Asks user if they want to add symbols 
   if (symbolInput == true){
-    alert('Symbols have been added.')
+    alert('Symbols have been added.') // Lets user know they have been added
     charLibrary += symbolOptionsLibrary; 
   }
   else {
-    alert('Symbols have not been added.')
+    alert('Symbols have not been added.') // Lets user know they have not been added.
   }
-  console.log('symbols option has been decided.');
+  console.log('symbols option has been decided.'); 
   return symbolInput;
 }
 
 
-// Generate the Password
-
+// Makes the Password
 function generatePassword() {
-  charLibrary = '';
-  var passwordLengthValue = passwordLength(8, 128);
+  charLibrary = ''; // Sets the CharLibrary to empty
+  var passwordLengthValue = passwordLength(8, 128); // Sets the parameters for how long the password should be. First value min, second value max
   if (passwordLengthValue === null) {
     return ""; // Return an empty string if the user cancelled the password length prompt
   }
@@ -129,13 +124,13 @@ function generatePassword() {
   symbolLibrary();
 
   if (charLibrary.length === 0) {
-    alert('There is a problem. You need to select at least one of the character options.');
-    // Call the character options again
-    return generatePassword();
+    alert('User error. You need to select at least one of the character options. Restarting password generator...'); // Alerts user theres an error, tells how to fix it and restarts password generator
+    return generatePassword(); // loops back into the Generate password since none of the libraries were selected
   }
 
-  var generatedPassword = "";
+  var generatedPassword = ""; // Clears out generate password
 
+  // for loop that generates each character randomly
   for (var i = 0; i < passwordLengthValue; i++) {
     generatedPassword += charLibrary.charAt(Math.floor(Math.random() * charLibrary.length));
   }
